5e361e21c5244d4545c10f262e22c34cc96aaea8
Reza Rafiei <86382022+rezaraf20@users.noreply.github.com>
2026-09-19 00:10:23 +0330

    fix: bootstrap Laravel kernel in feature tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,6 +9,9 @@ abstract class TestCase extends BaseTestCase
 {
     public function createApplication()
     {
-        return require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__.'/../bootstrap/app.php';
+        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
     }
 }
