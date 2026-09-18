37002504a3a1b867e63a6531513c4cac9a1c4843
Reza Rafiei <86382022+rezaraf20@users.noreply.github.com>
2026-09-18 21:56:32 +0330

    config: add telegram webhook secret

--- config/services.php
+++ config/services.php
@@ -14,6 +14,7 @@ return [
     ],
     'telegram' => [
         'bot_token' => env('TELEGRAM_BOT_TOKEN'),
+        'webhook_secret' => env('TELEGRAM_WEBHOOK_SECRET'),
     ],
     'haman_planner' => [
         'api_token' => env('APP_API_TOKEN'),
