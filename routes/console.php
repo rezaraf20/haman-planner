63843d1b00e2741d7feb28c7aa387622f65799dc
Reza Rafiei <86382022+rezaraf20@users.noreply.github.com>
2026-09-18 19:23:32 +0330

    feat: add reminder dispatch engine

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::call(function (): void {
-    // Haman Planner scheduled jobs will be registered here.
-})->dailyAt('03:00')->withoutOverlapping();
+Schedule::command('planner:reminders')
+    ->everyMinute()
+    ->withoutOverlapping();
