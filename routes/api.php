91915cae96fcac93a98875cf27cf23bb47cd3fc5
Reza Rafiei <86382022+rezaraf20@users.noreply.github.com>
2026-09-18 22:16:35 +0330

    feat: expose reminder api routes

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Api\DependencyController;
 use App\Http\Controllers\Api\ExecutionLogController;
 use App\Http\Controllers\Api\CommandController;
 use App\Http\Controllers\Api\ReviewController;
+use App\Http\Controllers\Api\ReminderController;
 use App\Http\Controllers\TelegramWebhookController;
 use App\Http\Middleware\ApiTokenMiddleware;
 
@@ -25,6 +26,10 @@ Route::middleware([ApiTokenMiddleware::class, 'throttle:120,1'])->group(function
     Route::post('/tasks/{task}/dependencies', [DependencyController::class, 'store']);
     Route::delete('/tasks/{task}/dependencies/{dependency}', [DependencyController::class, 'destroy']);
     Route::post('/tasks/{task}/execution-logs', [ExecutionLogController::class, 'store']);
+    Route::get('/reminders', [ReminderController::class, 'index']);
+    Route::post('/reminders', [ReminderController::class, 'store']);
+    Route::delete('/reminders/{reminder}', [ReminderController::class, 'destroy']);
+    Route::post('/reminders/{reminder}/cancel', [ReminderController::class, 'cancel']);
     Route::post('/commands', [CommandController::class, 'handle']);
     Route::get('/reviews', [ReviewController::class, 'index']);
     Route::post('/reviews/generate', [ReviewController::class, 'generate']);
