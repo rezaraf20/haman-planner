1d46053e0be08355f2437a2b52539641130d707f
Reza Rafiei <86382022+rezaraf20@users.noreply.github.com>
2026-09-18 19:09:20 +0330

    chore: add database configuration

---
+++ config/database.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'default' => env('DB_CONNECTION', 'pgsql'),
+    'connections' => [
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'haman_planner'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+        ],
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => true,
+        ],
+    ],
+    'migrations' => ['table' => 'migrations', 'update_date_on_publish' => true],
+];
