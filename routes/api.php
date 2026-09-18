fbdfd36cf97262053190ed6e7965006093c591eb
Reza Rafiei <86382022+rezaraf20@users.noreply.github.com>
2026-09-18 19:57:57 +0330

    feat: expose project and milestone APIs

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TaskController;
 use App\Http\Controllers\Api\GoalController;
+use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\MilestoneController;
 use App\Http\Controllers\Api\PlannerController;
 use App\Http\Controllers\Api\DependencyController;
 use App\Http\Controllers\Api\ExecutionLogController;
@@ -16,6 +18,8 @@ Route::post('/telegram/webhook', TelegramWebhookController::class);
 Route::middleware(ApiTokenMiddleware::class)->group(function (): void {
     Route::apiResource('tasks', TaskController::class);
     Route::apiResource('goals', GoalController::class);
+    Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('milestones', MilestoneController::class);
     Route::get('/planner/today', [PlannerController::class, 'today']);
     Route::get('/planner/analytics', [PlannerController::class, 'analytics']);
     Route::post('/tasks/{task}/dependencies', [DependencyController::class, 'store']);
