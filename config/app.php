b5a2591091589fda0a12deb2f2e36a49c3297d55
Reza Rafiei <86382022+rezaraf20@users.noreply.github.com>
2026-09-18 19:09:17 +0330

    chore: add Laravel configuration

---
+++ config/app.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'name' => env('APP_NAME', 'Haman Planner'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Tehran'),
+    'locale' => 'en',
+    'fallback_locale' => 'en',
+    'faker_locale' => 'en_US',
+    'key' => env('APP_KEY'),
+    'cipher' => 'AES-256-CBC',
+    'maintenance' => ['driver' => 'file'],
+];
